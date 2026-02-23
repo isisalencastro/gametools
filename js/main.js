bc049fec35457aef0422b073bf34b119001d3864
Isis Alencastro <91698364+isisalencastro@users.noreply.github.com>
2026-02-22 23:26:15 -0300

    feat: tornar home mais atrativa e corrigir experiências de imc e conversão

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
 // Bootstrap principal: inicializa módulos por domínio conforme elementos disponíveis na página.
+import { initCatalogExperience } from './common/catalog.js';
+import { initThemeToggle } from './common/theme.js';
 import { initGuessFeature } from './games/guess.js';
 import { initReactionFeature } from './games/reaction.js';
 import { initImcFeature } from './tools/imc.js';
 import { initPercentageFeature } from './tools/percentage.js';
 import { initTemperatureFeature } from './tools/temperature.js';
 
+initThemeToggle();
 initReactionFeature();
 initGuessFeature();
+initCatalogExperience();
 initImcFeature();
 initPercentageFeature();
 initTemperatureFeature();
